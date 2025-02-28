020bc23d4ad5a8bb7daefe717a2cfa6985777856
Molkobahn <stoffeosterhof@gmail.com>
2025-02-28 09:31:46 +0100

    Generating grid

---
+++ github.com/Molkobahn/maze_solver/maze.py
@@ -0,0 +1,50 @@
+from cell import Cell
+import time
+
+class Maze():
+    def __init__(
+            self,
+            x1,
+            y1,
+            num_rows,
+            num_cols,
+            cell_size_x,
+            cell_size_y,
+            win=None,
+            ):
+        self._cells = []
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+
+        self._create_cells()
+
+    def _create_cells(self):
+        for i in range(self.num_cols):
+            self._cells.append([])
+            for j in range(self.num_rows):
+                self._cells[i].append(Cell(self.win))
+        for i in range(self.num_cols):
+            for j in range(self.num_rows):
+                self._draw_cell(i, j)
+
+    def _draw_cell(self, i, j):
+        if self.win is None:
+            return
+        c = Cell(self.win)
+        cell_x1 = self.x1 + (self.cell_size_x * i)
+        cell_x2 = self.x1 + (self.cell_size_x * (i + 1))
+        cell_y1 = self.y1 + (self.cell_size_y * j)
+        cell_y2 = self.y1 + (self.cell_size_y * (j + 1))
+        c.draw(cell_x1, cell_y1, cell_x2, cell_y2)
+        self._animate()
+
+    def _animate(self):
+        if self.win is None:
+            return
+        self.win.redraw()
+        time.sleep(0.05)
